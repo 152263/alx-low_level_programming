39a0947ae19ce8fff85bb1d7a99e712dfba7e47d
Nwanneka Ernest Okoronkwo <ernestokoronkwo@gmail.com>
2022-09-09 13:38:48 -0700

    combination of brains and materials

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - combination of brains and materials
+ *
+ * Description: prints all possible different combinations of two digits.
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i, j;
+
+	i = 48;
+	j = 48;
+	while (i < 58)
+	{
+		j = 48;
+		while (j < 58)
+		{
+			putchar(i);
+			putchar(j);
+
+			if (i < 57 || j < 57)
+			{
+				putchar(44);
+				putchar(32);
+			}
+			j++;
+		}
+		i++;
+	}
+	putchar(10);
+	return (0);
+}
