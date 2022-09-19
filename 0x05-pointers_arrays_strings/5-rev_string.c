994b7fbe566b85af7ef7d4943d5f540c2f7aa316
Nwanneka Ernest Okoronkwo <ernestokoronkwo@gmail.com>
2022-09-19 07:33:16 -0700

    The fun in reversing

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * rev_string - Reverses a string.
@@ -6,16 +6,17 @@
  */
 void rev_string(char *s)
 {
-	int len = 0, index = 0;
-	char tmp;
+	int len, i, half;
+	char temp;
 
-	while (s[index++])
-		len++;
-
-	for (index = len - 1; index >= len / 2; index--)
+	for (len = 0; s[len] != '\0'; len++)
+	i = 0;
+	half = len / 2;
+	while (half--)
 	{
-		tmp = s[index];
-		s[index] = s[len - index - 1];
-		s[len - index - 1] = tmp;
+		temp = s[len - i - 1];
+		s[len - i - 1] = s[i];
+		s[i] = temp;
+		i++;
 	}
 }
