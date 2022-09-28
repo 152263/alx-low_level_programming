2cd3105f8bf9ab1805e3ea77619600018dd7495e
Nwanneka Ernest Okoronkwo <ernestokoronkwo@gmail.com>
2022-09-28 13:33:08 -0700

    puts with recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,8 +3,7 @@
 /**
  * _puts_recursion - prints a string, followed by a new line.
  * @s: string
- * Return: void._putchar(*s);
-	_puts_recursion(s + 1);
+ * Return: void
  */
 void _puts_recursion(char *s)
 {
