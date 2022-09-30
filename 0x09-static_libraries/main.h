967260ab652e4c68c4d532709d363cf33c111afc
Nwanneka Ernest Okoronkwo <ernestokoronkwo@gmail.com>
2022-09-30 16:18:09 -0700

    correction

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,5 +1,8 @@
-int _putchar(char c);
+#ifndef MAIN_H
+#define MAIN_H
+
 int _islower(int c);
+int _putchar(char c);
 int _isalpha(int c);
 int _abs(int n);
 int _isupper(int c);
@@ -18,3 +21,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+
+#endif
